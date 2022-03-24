d768919106b569779bafb45a80165a129eb17753
Nathan Yi <nathanyi112@gmail.com>
2022-03-23 22:21:04 -0500

    adds working api call

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-$.ajax(``)
+const $input = $('input')
+const $button = $('button')
+
+
+$.ajax(`https://www.cheapshark.com/api/1.0/games?title=batman&steamAppID=35140&limit=60&exact=0
+`).then((data) => {
+    console.log(data)
+})
