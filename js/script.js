0a1f33ed7095c0ebade50ede6cac4eb38a8a68e2
Nathan Yi <nathanyi112@gmail.com>
2022-03-23 23:08:54 -0500

    adds loop for images

--- js/script.js
+++ js/script.js
@@ -1,8 +1,30 @@
 const $input = $('input')
 const $button = $('button')
 
+$button.on("click", () => {
+  const gameName = $input.val()
+  $.ajax(
+    `https://www.cheapshark.com/api/1.0/games?title=${gameName}&limit=50&exact=0`
+  ).then((data) => {
+    console.log(data);
+    $(".results").html(`i worked`)
+    // let $img = $('<img id = "thumbnail">')
+    // $img.attr('src',data[0].thumb)
+    // $img.appendTo(".results")
+    for (let i = 0; i <= data.length; i++) {
+        let $img = $('<img id = "thumbnail">')
+        $img.attr('src',data[i].thumb)
+        $img.appendTo(".results")
+        console.log("hi")
+        console.log(data[i].thumb)
+    }
 
-$.ajax(`https://www.cheapshark.com/api/1.0/games?title=batman&steamAppID=35140&limit=60&exact=0
-`).then((data) => {
-    console.log(data)
-})
+
+  });
+});
+
+// $.ajax(`https://www.cheapshark.com/api/1.0/games?title=batman&steamAppID=35140&limit=60&exact=0
+// `).then((data) => {
+//     console.log(data)
+// })
+
