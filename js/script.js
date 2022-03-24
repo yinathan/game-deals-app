a8d19bf2c6c2d17f0603b558802706e6959d1dfc
Nathan Yi <nathanyi112@gmail.com>
2022-03-24 00:13:36 -0500

    adds separate divs in loop

--- js/script.js
+++ js/script.js
@@ -11,10 +11,15 @@ $button.on("click", () => {
     // $img.attr('src',data[0].thumb)
     // $img.appendTo(".results")
     for (let i = 0; i <= data.length; i++) {
-        $(".results").append(`<div class = ${i}></div>`)
+        const $div = $("<div></div>")
+        $div.attr('class', 'item')
+        let $gameName = data[i].external;
+        $div.html(`${data[i].external}`);
         let $img = $(`<img id = "thumbnail${i}">`)
         $img.attr('src',data[i].thumb)
-        $img.appendTo(`.${i}`)
+        $img.appendTo($div)
+        // $(".results").append(`<div class = "item"></div>`);
+        $(".results").append($div)
         
     }
 
